46a61a6b43f1e228dedfa317b42c67c356a80d40
Code With Azan <azanjokhio54@gmail.com>
2025-07-19 23:23:04 +0500

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blueAccent,
+          foregroundColor: Colors.white,
+          title: Center(
+            child: Text("Firebase Notification"),
+          ),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Center(
+              child: Text(
+                "Welcome to Firebase Push Notifications",
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
